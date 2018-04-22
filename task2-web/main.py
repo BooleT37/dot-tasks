d68937cd7da1ec497abef300b427551ff46d3c0d
Alexey Levin <BooleT37@gmail.com>
2018-04-22 20:41:26 +0500

    Task2: Swagger setup

---
+++ task2-web/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, send_from_directory, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/static/<path:path>')
+def send_static_file(path):
+    return send_from_directory('static', path)
+
+
+@app.route('/swagger-specs/<path:path>')
+def send_swagger_json(path):
+    return send_from_directory('swagger-specs', path)
+
+
+@app.route('/swagger')
+def swagger():
+    return send_file('./static/swagger.html')
+
+
+app.run(debug=True, use_debugger=False, use_reloader=False)
